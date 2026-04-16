b357b62640f2fa3294f14c20a03aec49dabd29d1
Saibal kole <saibalkole@gmail.com>
2026-04-16 23:59:37 +0530

    feat: add fetchMovie, fetchMovieByGenre, and movieDetailsUrl endpoints to API constants

--- constants/api.ts
+++ constants/api.ts
@@ -6,4 +6,10 @@ export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 export const ENDPOINTS = {
   getMovies: `${API_BASE_URL}/movies`,
   getMovieDetails: (id: string) => `${API_BASE_URL}/movies/${id}`,
+  fetchMovie: (pageNumber: number) =>
+    `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc`,
+  fetchMovieByGenre: (genreId: number, pageNumber: number) =>
+    `${API_BASE_URL}/discover/movie?with_genres=${genreId}&include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc`,
+  movieDetailsUrl: (movieId: string) =>
+    `${API_BASE_URL}/movie/${movieId}?language=en-US`,
 };
