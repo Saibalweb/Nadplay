2e8d5f90ba7e237a6453d7a88c91f7334c09504b
Saibal kole <saibalkole@gmail.com>
2026-04-17 23:56:35 +0530

    bug fixes

--- constants/api.ts
+++ constants/api.ts
@@ -4,11 +4,11 @@ export const API_IMAGE_URL = process.env.EXPO_PUBLIC_Image_URL;
 export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
 export const ENDPOINTS = {
-  getMovies: `${API_BASE_URL}/movies`,
-  getMovieDetails: (id: string) => `${API_BASE_URL}/movies/${id}`,
-  fetchMovie: (pageNumber: number) =>
+  getMoviesUrl: `${API_BASE_URL}/movies`,
+  getMovieDetailsUrl: (id: string) => `${API_BASE_URL}/movies/${id}`,
+  fetchMovieUrl: (pageNumber: number) =>
     `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc`,
-  fetchMovieByGenre: (genreId: number, pageNumber: number) =>
+  fetchMovieByGenreUrl: (genreId: number, pageNumber: number) =>
     `${API_BASE_URL}/discover/movie?with_genres=${genreId}&include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc`,
   movieDetailsUrl: (movieId: string) =>
     `${API_BASE_URL}/movie/${movieId}?language=en-US`,
